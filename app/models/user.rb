50fb82ae2d4dd9f17544540655618861c1f11e80
Jens Jap <jensjap@gmail.com>
2013-04-15 13:38:13 -0400

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+    attr_accessible(:name, :email, :password, :password_confirmation)
+    has_secure_password
+
+    #before_save { |user| user.email = email.downcase }
+    before_save { email.downcase! }
+
+    validates(:name, { :presence => true })
+    validates(:name, { :length => { :maximum => 50 } })
+    validates :email, presence: true    # Just for kicks I'm using two
+                                        # different conventions/styles.
+                                        # In ruby parentheses are optional
+                                        # when passing parameters to a
+                                        # function and if the last parameter
+                                        # is a hash then the braces are
+                                        # also optional
+    VALID_EMAIL_REGEX = /\A[\w]+[\w+\-.]*@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX },
+        uniqueness: { case_sensitive: false }   # Notice that you can chain validations.
+                                                # This seems to work only with the new
+                                                # hash style
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
+end
