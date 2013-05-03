089807e364acbbbab0b39c9052e2714c8246290b
Jens Jap <jens_jap@brown.edu>
2013-05-03 11:05:25 -0400

    Tests aren't passing because visit user_path(user) raises NoMethodError.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -26,4 +26,36 @@ describe Micropost do
         before { @micropost.user_id = nil }
         it { should_not be_valid }
     end
+
+    describe "when user_id is not present" do
+        before { @micropost.user_id = nil }
+        it { should_not be_valid }
+    end
+
+    describe "with blank content" do
+        before { @micropost.content = " " }
+        it { should_not be_valid }
+    end
+
+    describe "with content that is too long" do
+        before { @micropost.content = "a" * 141 }
+        it { should_not be_valid }
+    end
+
+    describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+        let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+        let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+        before { visit user_path(user) }
+
+        it { should have_selector('h1',    text: user.name) }
+        it { should have_selector('title', text: user.name) }
+
+        describe "microposts" do
+            it { should have_content(m1.content) }
+            it { should have_content(m2.content) }
+            it { should have_content(user.microposts.count) }
+        end
+    end
 end
